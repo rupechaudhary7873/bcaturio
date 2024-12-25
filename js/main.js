d33699eca78ca86bc87ea2b54291581986224f22
Rupnarayan <rupechaudhary673@gmail.com>
2024-12-25 18:47:47 +0545

    #add initial file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".banner-slider").owlCarousel({items:1,
+        autoplay:true,
+        nav: true,
+        navText: ['<i class="ri-arrow-up-line"></i>','<i class="fa-solid fa-arrow-down-long"></i>'],
+        dotsContainer: true
+        
+    });
+  });
