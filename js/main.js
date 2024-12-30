449950593aeeb177c3d25830d2b711e9a5a5a5d6
Rupnarayan <rupechaudhary673@gmail.com>
2024-12-30 19:41:53 +0545

    #update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ $(document).ready(function(){
         autoplay:true,
         nav: true,
         navText: ['<i class="ri-arrow-up-line"></i>','<i class="fa-solid fa-arrow-down-long"></i>'],
-        dotsContainer: true
+        dotsContainer: true,
         
     });
   });
+  $(".card-slider").owlCarousel({items:3,
+        loop:true
+});
+
